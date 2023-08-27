ee01f0ffb0f20ba8cc4e72f60399cfccd4af051a
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-08-27 15:11:43 +0900

    commit

--- functions/src/between/index.ts
+++
@@ -1,5 +0,0 @@
-import * as gpt35 from './gpt35';
-
-export const v1 = {
-  Gpt35: { ...gpt35 },
-};
--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as between from './between';
-
-// initialize
-admin.initializeApp();
-
-export { between };
